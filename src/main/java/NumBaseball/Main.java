efc1ee13afb39f6ac398b9472e432f08151e6045
Hyunta <mohai2618@naver.com>
2021-08-17 18:11:53 +0900

    3스트라이크일 경우 종료

--- src/main/java/NumBaseball/Main.java
+++ src/main/java/NumBaseball/Main.java
@@ -4,26 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String nums = sc.nextLine();
-        String ex = "123";
-        System.out.println("nums = " + nums.charAt(0));
-        checkBS(nums, ex);
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String nums = sc.nextLine();
+            String ex = "123";
+            System.out.println("nums = " + nums.charAt(0));
+            checkBS(nums, ex);
+        }
     }
 
     private static void checkBS(String nums, String ex) {
-        int Ball = 0;
-        int Strike = 0;
+        int ball = 0;
+        int strike = 0;
         for (int i = 0; i < 3; i++) {
             if (nums.charAt(i) == ex.charAt(i)) {
-                Strike += 1;
+                strike += 1;
                 continue;
             }
             if (ex.indexOf(nums.charAt(i))!= -1) {
-                Ball += 1;
+                ball += 1;
             }
         }
-        System.out.println("Strike = " + Strike);
-        System.out.println("Ball = " + Ball);
+        System.out.println("Strike = " + strike);
+        System.out.println("Ball = " + ball);
+        if (strike == 3) {
+            System.exit(0);
+        }
     }
 }
