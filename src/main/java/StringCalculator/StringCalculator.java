ee3f65064d850b878c4c37993613ae368b5ede4a
Hyunta <mohai2618@naver.com>
2021-08-17 16:56:24 +0900

    여러 숫자에 대한 계산 구현

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -1,24 +1,25 @@
 package StringCalculator;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class StringCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringCalculator cal = new StringCalculator();
         System.out.println("수식을 입력하세요.");
         String[] values = sc.nextLine().split(" ");
 
         int first = Integer.parseInt(values[0]);
-        System.out.println("first = " + first);
-        String symbol = values[1];
-        System.out.println("symbol = " + symbol);
-        int second = Integer.parseInt(values[2]);
-        System.out.println("second = " + second);
+        int answer = first;
 
-        int answer = calculate(first, symbol, second);
+        int i = 1;
+        while (i < values.length) {
+            String symbol = values[i];
+            int second = Integer.parseInt(values[i+1]);
+            answer = calculate(answer, symbol, second);
+            i += 2;
+        }
+
         System.out.println("answer = " + answer);
     }
 
