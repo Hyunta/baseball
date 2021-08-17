4c243fd015ae7bdb6554579ca4b3d176d2af5551
Hyunta <mohai2618@naver.com>
2021-08-17 18:51:14 +0900

    중복하지 않는 세자리 난수 생성

--- src/main/java/NumBaseball/Main.java
+++ src/main/java/NumBaseball/Main.java
@@ -1,16 +1,38 @@
 package NumBaseball;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ex = RandomNumber();
+
+
         while (true) {
-            Scanner sc = new Scanner(System.in);
+            System.out.print("숫자를 입력해 주세요 :");
             String nums = sc.nextLine();
-            String ex = "123";
-            System.out.println("nums = " + nums.charAt(0));
+            System.out.println(nums);
             checkBS(nums, ex);
         }
+    }
+
+    private static String RandomNumber() {
+        StringBuilder sb = new StringBuilder();
+        Set<Integer> set = new HashSet<>();
+        while (sb.length() != 3) {
+            int num = 0;
+            num = (int) (Math.random() * 9) + 1;
+            if (set.contains(num)) {
+                continue;
+            } else {
+                sb.append(num);
+                set.add(num);
+            }
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 
     private static void checkBS(String nums, String ex) {
@@ -25,10 +47,27 @@ public class Main {
                 ball += 1;
             }
         }
-        System.out.println("Strike = " + strike);
-        System.out.println("Ball = " + ball);
+        System.out.println(CallScore(ball, strike));
         if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.exit(0);
         }
     }
-}
+
+    private static String CallScore(int ball, int strike) {
+        if (strike == 0 && ball == 0) {
+            return "OUT!";
+        }
+        if (strike == 0 && ball != 0) {
+            return (ball + "볼");
+        }
+        if (strike != 0 && ball == 0) {
+            return (strike + "스트라이크");
+        }
+
+        if (strike != 0 && ball != 0) {
+            return (ball + "볼 " + strike + "스트라이크");
+        }
+        return null;
+        }
+    }
