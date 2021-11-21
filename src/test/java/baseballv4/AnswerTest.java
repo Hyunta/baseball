fa5a2ca8f7cb75ae7c197381bd68a34cfe1f0a98
Hyunta <mohai2618@naver.com>
2021-11-21 17:05:57 +0900

    test(Answer): Random 한 3자리 숫자가 Unique한 값이 나오는지 확인

---
+++ src/test/java/baseballv4/AnswerTest.java
@@ -0,0 +1,14 @@
+package baseballv4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnswerTest {
+    Answer answer = new Answer();
+
+    @Test
+    void isUnique() {
+        answer.makeAnswer();
+    }
+}
